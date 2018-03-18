9d6446a76079860d91072752e314f6e0813d2edb
huang <javahuang1995@163.comm>
2018-03-18 19:50:25 +0800

    commit

--- java/java_设计模式/适配器模式/adapter2/ThreePinSocket.java
+++
@@ -1,6 +0,0 @@
-package com.huang.adapter2;
-
-public interface ThreePinSocket {
-	public void chargeWithThreePin();
-	public int voltage();
-}
--- java/java_设计模式/适配器模式/adapter2/TwoPinSocket.java
+++
@@ -1,6 +0,0 @@
-package com.huang.adapter2;
-
-public interface TwoPinSocket {
-	public void chargeWithTwoPin();
-	public int voltage();
-}
