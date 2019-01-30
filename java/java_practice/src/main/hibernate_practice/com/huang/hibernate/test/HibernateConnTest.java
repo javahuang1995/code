d752234f52b9d6628ae331c359325382476e12ef
huang <1468435744@qq.com>
2019-01-30 10:01:04 +0800

    ddfddds

--- java/java_practice/src/main/hibernate_practice/com/huang/hibernate/test/HibernateConnTest.java
+++ java/java_practice/src/main/hibernate_practice/com/huang/hibernate/test/HibernateConnTest.java
@@ -1,4 +1,4 @@
-package com.huang.mybatis.test;
+package com.huang.hibernate.test;
 import javax.annotation.Resource;
 
 import org.apache.ibatis.session.SqlSession;
@@ -10,12 +10,18 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.taotao.mapper.TbItemMapper;
-import com.taotao.pojo.TbItem;
+import cn.it.shop.dao.AccountDao;
+import cn.it.shop.dao.BaseDao;
+import cn.it.shop.dao.CategoryDao;
+import cn.it.shop.dao.ForderDao;
+import cn.it.shop.dao.ProductDao;
+import cn.it.shop.dao.SorderDao;
+import cn.it.shop.dao.UserDao;
+import cn.it.shop.service.BaseService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="classpath:spring/applicationContext.xml")
-public class ConnTest {
+public class HibernateConnTest {
 
 	@Resource
 	private SessionFactory sessionFactory;
@@ -23,8 +29,19 @@ public class ConnTest {
 	@Resource
 	private SqlSessionFactory sqlsessionFactory;
 	
+	private BaseDao baseDao;
 	@Resource
-	private TbItemMapper itemMapper;
+	private AccountDao accountDao;
+	@Resource
+	private CategoryDao categoryDao;
+	@Resource
+	private ForderDao forderDao;
+	@Resource
+	private ProductDao productDao;
+	@Resource
+	private SorderDao sorderDao;
+	@Resource
+	private UserDao userDao;
 	
 	
 	
@@ -42,8 +59,7 @@ public class ConnTest {
 		SqlSession s = sqlsessionFactory.openSession();
 		System.out.println(s);
 		
-		TbItem a = itemMapper.selectByPrimaryKey((long) 536563);
-		System.out.println(a.getTitle());
+		
 		
 	}
 
--- java/java_practice/src/main/mybatis_practice/com/huang/mybatis/test/MybatisConnTest.java
+++ java/java_practice/src/main/mybatis_practice/com/huang/mybatis/test/MybatisConnTest.java
@@ -1,4 +1,4 @@
-package com.huang.hibernate.test;
+package com.huang.mybatis.test;
 import javax.annotation.Resource;
 
 import org.apache.ibatis.session.SqlSession;
@@ -10,18 +10,12 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import cn.it.shop.dao.AccountDao;
-import cn.it.shop.dao.BaseDao;
-import cn.it.shop.dao.CategoryDao;
-import cn.it.shop.dao.ForderDao;
-import cn.it.shop.dao.ProductDao;
-import cn.it.shop.dao.SorderDao;
-import cn.it.shop.dao.UserDao;
-import cn.it.shop.service.BaseService;
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="classpath:spring/applicationContext.xml")
-public class ConnTest {
+public class MybatisConnTest {
 
 	@Resource
 	private SessionFactory sessionFactory;
@@ -29,19 +23,8 @@ public class ConnTest {
 	@Resource
 	private SqlSessionFactory sqlsessionFactory;
 	
-	private BaseDao baseDao;
 	@Resource
-	private AccountDao accountDao;
-	@Resource
-	private CategoryDao categoryDao;
-	@Resource
-	private ForderDao forderDao;
-	@Resource
-	private ProductDao productDao;
-	@Resource
-	private SorderDao sorderDao;
-	@Resource
-	private UserDao userDao;
+	private TbItemMapper itemMapper;
 	
 	
 	
@@ -59,7 +42,8 @@ public class ConnTest {
 		SqlSession s = sqlsessionFactory.openSession();
 		System.out.println(s);
 		
-		
+		TbItem a = itemMapper.selectByPrimaryKey((long) 536563);
+		System.out.println(a.getTitle());
 		
 	}
 
