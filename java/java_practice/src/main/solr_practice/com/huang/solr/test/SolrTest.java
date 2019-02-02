1dba92146ae20b4f0b1763bafe114a517c3ec344
huang <1468435744@qq.com>
2019-02-02 16:26:47 +0800

    公开：    表示 对外说明的日志 新增：    表示 新增加的功能 更新：    表示 优化更新的功能 bug修复： 表示 修复一些原来存在的bug 测试：    表示 增加测试用例 重构：    表示 重构（即不是新增功能，也不是修改bug的代码变动） 格式：    表示 不影响代码运行的变动 配置：    表示 配置文件的修改，非代码修改 文档：    表示 文档上传 详细描述：表示 对这次提交内容的详细说明 author：  中国共产党万岁

--- java/java_practice/src/main/solr_practice/com/huang/solr/test/SolrTest.java
+++ java/java_practice/src/main/solr_practice/com/huang/solr/test/SolrTest.java
@@ -1,11 +1,14 @@
 package com.huang.solr.test;
 
+import java.util.List;
+
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.SolrInputDocument;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -13,14 +16,50 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.taotao.mapper.ItemMapper;
+import com.taotao.pojo.Item;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:spring/applicationContext-solr.xml")
+@ContextConfiguration(locations = "classpath:spring/applicationContext-*.xml")
 public class SolrTest {
 
 	private static final Logger logger = LoggerFactory.getLogger(SolrTest.class);
 	@Autowired
 	private SolrServer solrServer;
+	@Autowired
+	private ItemMapper itemMapper;
+
+	/**
+	 * 这里就是涉及到2个操作
+	 * 1.把数据从数据库里面读出来
+	 * 2.读出来的数据上传到solr服务器里面。就完事了。
+	 */
+	@Test
+	public void testImport() {
+		try {
+
+			// 查询商品列表
+			List<Item> list = itemMapper.getItemList();
+			// 把商品信息写入索引库
+			for (Item item : list) {
+				// 创建一个SolrInputDocument对象
+				SolrInputDocument document = new SolrInputDocument();
+				document.setField("id", item.getId());
+				document.setField("item_title", item.getTitle());
+				document.setField("item_sell_point", item.getSell_point());
+				document.setField("item_price", item.getPrice());
+				document.setField("item_image", item.getImage());
+				document.setField("item_category_name", item.getCategory_name());
+				document.setField("item_desc", item.getItem_des());
+				// 写入索引库
+				solrServer.add(document);
+			}
+			// 提交修改
+			solrServer.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Test
 	public void testQuery() {
