51196ca002725ddc969314eb2dbee50b9d535183
huang <1468435744@qq.com>
2019-02-25 22:15:45 +0800

    提交人: 老子我 提交描述：xxx author：  中国共产党万岁

--- sql/windows/test/taobao.sql
+++ sql/windows/test/taobao.sql
@@ -1,3 +1,3 @@
-SELECT * from tb_item t where exists(SELECT t.cid from tb_item_cat t where t.title='手机');
+SELECT * from tb_item t where exists(select * from tb_item where t);
 
 SELECT * from tb_item t where t.cid in(76,298);
--- sql/windows/test/taobao.sql.bak
+++
@@ -1,3 +0,0 @@
-SELECT * from tb_item t where exists(select * from tb_item where t);
-
-SELECT * from tb_item t where t.cid in(76,298);
