dceb1a3717b8688ca56e18279ad50d9ae1abacf3
huang <1468435744@qq.com>
2019-03-01 22:22:39 +0800

    cc

--- sql/windows/test/master test.sql
+++
@@ -1,7 +0,0 @@
-grant replication slave,reload,super ON *.* TO 'root'@'192.168.0.%' IDENTIFIED BY '1995';
-flush privileges;
-
-show master status ;
-
-
-insert into test.huang_hehe value('444');commit;
